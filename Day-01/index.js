2a6994f8736b7510bc450978d6f4c021f04a641a
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-01-10 14:56:32 +0530

    Day-01 code added

---
+++ Day-01/index.js
@@ -0,0 +1,6 @@
+console.log("Hello World, Welcome to WSCube Tech Backend Development Course!");
+console.log(20 + 10);
+
+let arr = [1, 2, 3, 4, 5, 6];
+arr.forEach((item, idx) => console.log(item, idx));
+
